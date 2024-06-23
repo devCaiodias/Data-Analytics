21edb21652f1ea65ae6853f3f80de2eebc54969e
Devcaiodias <caiodiasmartins12@gmail.com>
2024-06-23 15:42:49 -0300

    DataFrames - Preenchimento de Nulos

---
+++ Pandaspy/classroom19.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import pyarrow
+from pathlib import Path
+
+TIPO_DADOS = 'amostra'
+
+LINK_DADOS = {
+    "amostra" : {
+        "escola.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\escola.parquet",
+        "ideb.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\ideb.parquet",
+        "turma.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\turma.parquet"
+    }, 
+    "completo" : {
+        "escola.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\escola.parquet",
+        "ideb.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\ideb.parquet",
+        "turma.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\turma.parquet"
+    }
+}
+
+vPATH_DADOS = Path(f"C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\{TIPO_DADOS}")
+
+def carrega_parent(nome: str, pasta: str, **kwargs) -> pd.DataFrame:
+    return pd.read_parquet(vPATH_DADOS / f"{pasta}\\{nome}.parquet", **kwargs)
+
+
+df1 = carrega_parent("escola", "aquisicao", engine='pyarrow', filters=[("ANO", "=", 2020)])
+df2 = carrega_parent("turma", "aquisicao", engine='pyarrow', filters=[("ANO", "=", 2020)])
+df3 = carrega_parent("ideb", "aquisicao", engine='pyarrow')
+
+print(df3.reindex(columns=["ID_ESCOLA", "ANO", "IDEB_AI", "IDEB_AF"]).fillna(-1))
+print(df3.reindex(columns=["ID_ESCOLA", "ANO", "IDEB_AI", "IDEB_AF"]).ffill())
+print(df3.reindex(columns=["ID_ESCOLA", "ANO", "IDEB_AI", "IDEB_AF"]).bfill())
+print(df3.reindex(columns=["IDEB_AI", "IDEB_AF"]).ffill(axis=1))
