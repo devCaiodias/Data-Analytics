1748741807e153a3c1e404952cdcf7fd40965854
Devcaiodias <caiodiasmartins12@gmail.com>
2024-06-25 10:01:22 -0300

    DataFrames - Melt

---
+++ Pandaspy/classroom23.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import pyarrow
+from pathlib import Path
+
+TIPO_DADOS = 'amostra'
+
+LINK_DADOS = {
+    "amostra" : {
+        "escola.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\escola.parquet",
+        "ideb.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\ideb.parquet",
+        "turma.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\amostra\\aquisicao\\turma.parquet"
+    }, 
+    "completo" : {
+        "escola.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\escola.parquet",
+        "ideb.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\ideb.parquet",
+        "turma.parquet" : "C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\completo\\aquisicao\\turma.parquet"
+    }
+}
+
+vPATH_DADOS = Path(f"C:\\Users\\caio\\Data Analytics\\Pandaspy\\dados\\{TIPO_DADOS}")
+
+def carrega_parent(nome: str, pasta: str, **kwargs) -> pd.DataFrame:
+    return pd.read_parquet(vPATH_DADOS / f"{pasta}\\{nome}.parquet", **kwargs)
+
+
+df1 = carrega_parent("escola", "aquisicao", engine='pyarrow', filters=[("ANO", "=", 2020)])
+df2 = carrega_parent("turma", "aquisicao", engine='pyarrow', filters=[("ANO", "=", 2020)])
+df3 = carrega_parent("ideb", "aquisicao", engine='pyarrow')
+
+print(
+    (
+        df3.melt(
+            id_vars=["ID_ESCOLA", "ANO"],
+            value_vars=["IDEB_AI", "IDEB_AF", "IDEB_EM"],
+            var_name="TIPO_IDEB",
+            value_name="NOTA"
+        )
+        .assign(TIPO_IDEB=lambda f: f["TIPO_IDEB"].str[5:])
+        .dropna()
+        # .groupby(["ANO", "TIPO_IDEB"])["NOTA"].mean()
+        .pivot_table(index=["TIPO_IDEB"], columns="ANO" , values="NOTA", aggfunc="mean")
+    ))
