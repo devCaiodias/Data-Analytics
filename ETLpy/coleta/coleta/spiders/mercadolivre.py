27e428c987ce01f127e60a111bb34cae95d6f723
Devcaiodias <caiodiasmartins12@gmail.com>
2024-07-08 16:04:40 -0300

    Fist Project

---
+++ ETLpy/coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-masculino#D[A:tenis%20masculino]"]
+
+    def parse(self, response):
+        pass
